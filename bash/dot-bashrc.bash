9fd658e295d7daeabc6d5a86fee74d4a884d130b
Ren Odion <smakey18@gmail.com>
2024-05-29 20:24:19 -0700

    Add ssh support for gpg

--- bash/dot-bashrc.bash
+++ bash/dot-bashrc.bash
@@ -42,9 +42,15 @@ if [[ -f "/run/.containerenv" ]] ; then
     fi
 fi
 
-#GPG allow input of passphrase in tty
+# GPG allow input of passphrase in tty
 TTY=$(tty)
 export GPG_TTY=$TTY
+# Use GPG as ssh-agent
+unset SSH_AGENT_PID
+if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+  export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+fi
+gpg-connect-agent updatestartuptty /bye >/dev/null
 
 # Customize prompt
 MAGENTA="\[$(tput setaf 5)\]"
